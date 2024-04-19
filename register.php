f4ff59cebd4b958712b51ab6814ccf665777b37a
samarth mehra <112699079+samarthmehra10@users.noreply.github.com>
2024-04-19 07:04:02 +0530

    Update register.php

--- register.php
+++ register.php
@@ -1,3 +1,41 @@
+<?php
+require_once "dbconnection.php";
+require_once "session.php";
+
+if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['submit'])) {
+
+    $fullname = $_POST['name'];
+    $phn = $_POST['ph'];
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+    $confirm_password = $_POST['confirm_password'];
+    if($password==$confirm_password){
+
+    $qry = "INSERT INTO `users` (`email`, `name`, `pnumber`) VALUES ('$email', '$fullname', '$phn')";
+    $run = mysqli_query($dbcon,$qry);
+    
+    if($run==true){
+
+        $psqry = "INSERT INTO `login` (`email`, `password`, `u_id`) VALUES ('$email', '$password',LAST_INSERT_ID() )";
+        $psrun = mysqli_query($dbcon,$psqry);
+        ?>  <script>
+            alert('Registration Successfully :)'); 
+            window.open('index.php','_self');
+            </script>
+        <?php
+    }
+    }else{
+        ?>  <script>
+            alert('Password mismatched!!'); 
+            </script>
+        <?php
+    }
+
+}   
+?> 
+
+
+
 <!DOCTYPE html>
 <html lang="en">
 
